59dc23766f230aa9617ef92016658115ddae1278
iGAL Roytblat <igalr@redleafsolutions.ca>
2024-08-18 01:14:56 -0400

    Add row data and form entries

--- FileUpload/Code.js
+++ FileUpload/Code.js
@@ -35,7 +35,8 @@ const GSFileUpload = {
   launchUploadPage() {
     let scriptURL = "https://script.google.com/a/macros/redleafsolutions.ca/s/AKfycbzgc-qOKU1GZtdtG7eGPyet8WdJ7RkawI1CENF1g0LpEFkwn1G-WoTKcDTCQmquK71X/exec";
     let ssheetid = encodeURI(SpreadsheetApp.getActive().getId());
-    let sheetname = encodeURI(SpreadsheetApp.getActiveSheet().getName());
+    let sheet = SpreadsheetApp.getActiveSheet();
+    let sheetname = encodeURI(sheet.getName());
     let range = SpreadsheetApp.getActiveRange();
     if (range.getNumColumns() * range.getNumRows() != 1) {
       SpreadsheetApp.getUi().alert("You need to select only one blank cell");
@@ -47,8 +48,18 @@ const GSFileUpload = {
     }
     let notation = encodeURI(range.getA1Notation());
 
+    let header = sheet.getRange (1, 1, 1, sheet.getDataRange().getNumColumns()).getValues ()[0];
+    let row = sheet.getRange (range.getRow (), 1, range.getRow(), sheet.getDataRange().getNumColumns()).getValues ()[0];
+    let r = {};
+    for (let i in header) {
+      if (header[i] != '') {
+        r[header[i]] = row[i];
+      }
+    }
+    r = encodeURI (JSON.stringify (r));
+
     let html = '<html><body><script>';
-    html += `window.open ("${scriptURL}?ssheetid=${ssheetid}&sheetname=${sheetname}&range=${notation}", "_blank");`;
+    html += `window.open ("${scriptURL}?ssheetid=${ssheetid}&sheetname=${sheetname}&range=${notation}&row=${r}", "_blank");`;
     html += 'google.script.host.close();';
     html += '</script></body></html>';
     SpreadsheetApp.getUi().showModelessDialog(HtmlService.createHtmlOutput(html).setHeight(1).setWidth(1), " ");
@@ -92,6 +103,7 @@ function doGet(e) {
   Logger.log("Loading form " + formFile);
   let template = HtmlService.createTemplateFromFile(formFile);
   template.params = e.parameter;
+  template.row = e.parameter.row;
   return template.evaluate();
 }
 
